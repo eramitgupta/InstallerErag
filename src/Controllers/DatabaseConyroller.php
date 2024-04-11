bd81cfe730c30cda009fe549a67e99235e67009f
krupal vasoya <146057473+krupalinfyom@users.noreply.github.com>
2024-04-11 17:29:01 +0530

    Update DatabaseConyroller.php
    Changes in DatabaseConyroller

--- src/Controllers/DatabaseConyroller.php
+++ src/Controllers/DatabaseConyroller.php
@@ -13,6 +13,7 @@ use InstallerErag\Main\EnvironmentManager;
 
 class DatabaseConyroller extends Controller
 {
+    public $entries;
      /**
       * @var EnvironmentManager
       */
@@ -43,18 +44,30 @@ class DatabaseConyroller extends Controller
 
           if (!$this->checkDatabaseConnection($request)) {
                return $redirect->route('database_import')->withInput()->withErrors([
-                    'database_connection' => 'db connection failed',
+                    'database_hostname' => 'database connection failed',
                ]);
           }
 
-          $this->EnvironmentManager->saveFileWizard($request);
+        $this->readEnv();
+        $input = $request->only(['app_name', 'app_url', 'database_hostname', 'database_connection', 'database_port', 'database_name', 'database_username', 'database_password']);
 
-          return redirect(route('finish'));
+            $this->changeEntry('APP_NAME', $input['app_name']);
+            $this->changeEntry('APP_URL', $input['app_url']);
+            $this->changeEntry('DB_CONNECTION', $input['database_connection']);
+            $this->changeEntry('DB_HOST', $input['database_hostname']);
+            $this->changeEntry('DB_PORT', $input['database_port']);
+            $this->changeEntry('DB_DATABASE', $input['database_name']);
+            $this->changeEntry('DB_USERNAME', $input['database_username']);
+            $this->changeEntry('DB_PASSWORD', $input['database_password']);
+
+        //   $this->EnvironmentManager->saveFileWizard($request);
+
+            return redirect()->route('account');
      }
 
      private function checkDatabaseConnection(Request $request)
      {
-          $connection = $request->input('database_connection');
+          $connection = 'mysql';
 
           $settings = config("database.connections.$connection");
 
@@ -84,4 +97,50 @@ class DatabaseConyroller extends Controller
                return false;
           }
      }
+
+     public function readEnv()
+    {
+        $content = file_get_contents(base_path('.env'));
+
+        $lines = explode(PHP_EOL, $content);
+
+        foreach ($lines as $line) {
+            if (str_contains($line, '=')) {
+                $entry = explode('=', $line);
+                $value = $entry[1];
+                if (str_starts_with($value, '"') && str_ends_with($value, '"')) {
+                    $value = substr($value, 1, -1);
+                }
+                $this->entries[$entry[0]] = $value;
+            }
+        }
+    }
+
+    public function getEntry($key, $default = null)
+    {
+        return $this->entries[$key] ?? $default;
+    }
+
+    public function changeEntry($key, $value, $write = true)
+    {
+        $this->entries[$key] = $value;
+
+        if ($write) {
+            $this->writeEnv();
+        }
+    }
+
+    public function writeEnv()
+    {
+        $content = '';
+
+        foreach ($this->entries as $key => $value) {
+            if (str_contains($value, ' ')) {
+                $value = '"'.$value.'"';
+            }
+            $content .= "{$key}={$value}".PHP_EOL;
+        }
+
+        file_put_contents(base_path('.env'), $content);
+    }
 }
