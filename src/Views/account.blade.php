759312ed492d8c7ba54b5df6fdee8caf9e60f9c4
krupal vasoya <146057473+krupalinfyom@users.noreply.github.com>
2024-04-11 17:07:46 +0530

    Update account.blade.php
    Add: email setup

--- src/Views/account.blade.php
+++ src/Views/account.blade.php
@@ -1,41 +1,65 @@
-{{-- @section('title', 'Account')
+@section('title', 'Mail Setup')
 @extends('vendor.InstallerEragViews.app-layout')
 @section('content')
     <section class="mt-4">
         <div class="container">
-            <div class="col-md-6 cs_center">
-                <form action="{{ route('saveAccount') }}" method="post" class="card">
-                    @csrf
-                    <div class="card-body">
-                        <div class="tab">
-
-                            <div class="col-md-12 mb-3">
-                                <x-input label="Name" required="ture" name="first_name" type="text"
-                                    value="{{ old('first_name') }}" />
-                                <x-error for="first_name" />
+            <form action="{{ route('saveAccount') }}" method="post" class="card">
+                @csrf
+                <div class="card-body">
+                    <div class="tab">
+                        <div class="row">
+                            <div class="col-md-4 mb-3">
+                                <x-input label="Driver" name="mail_driver" type="text"
+                                    value="{{ old('mail_driver') }}" placeholder="smtp" />
+                                <x-error for="mail_driver" />
                             </div>
 
-                            <div class="col-md-12 mb-3">
-                                <x-input label="Email" required="ture" name="email" type="email"
-                                    value="{{ old('email') }}" />
-                                <x-error for="email" />
+                            <div class="col-md-4 mb-3">
+                                <x-input label="Host" name="mail_host" type="text"
+                                    value="{{ old('mail_host') }}" placeholder="localhost" />
+                                <x-error for="mail_host" />
                             </div>
-
-                            <div class="col-md-12 mb-3">
-                                <x-input label="Password" required="ture" name="password" type="password"
-                                    value="{{ old('password') }}" />
-                                <x-error for="password" />
+                            <div class="col-md-4 mb-3">
+                                <x-input label="Port" name="mail_port" type="text"
+                                    value="{{ old('mail_port') }}" placeholder="587 or 465 or 25" />
+                                <x-error for="mail_port" />
                             </div>
-
+                            <div class="col-md-4 mb-3">
+                                <x-input label="Mail Encryption" name="mail_encryption" type="text"
+                                    value="{{ old('mail_encryption') }}" placeholder="TLS or SSL or null" />
+                                <x-error for="mail_encryption" />
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <x-input label="Email Username" name="mail_username" type="email"
+                                    value="{{ old('mail_username') }}" placeholder="admin@example.com" />
+                                <x-error for="mail_username" />
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <x-input label="Email Password" name="mail_password" type="password"
+                                    value="{{ old('mail_password') }}" />
+                                <x-error for="mail_password" />
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <x-input label="Email From Address" name="mail_from_address" type="email"
+                                    value="{{ old('mail_from_address') }}" />
+                                <x-error for="mail_from_address" />
+                            </div>
+                            <div class="col-md-4 mb-3">
+                                <x-input label="Email From Name" name="mail_from_name" type="text"
+                                    value="{{ old('mail_from_name') }}" />
+                                <x-error for="mail_from_name" />
+                            </div>
                         </div>
                     </div>
-                    <div class="card-footer text-end">
-                        <div class="d-flex">
-                            <button type="submit" id="next_button" class="btn btn-primary ms-auto">Next</button>
-                        </div>
+                </div>
+                <div class="card-footer text-end">
+                    <div class="d-flex">
+                        <button type="submit" id="next_button" class="btn btn-primary ms-auto">Next</button>
+                        {{-- <a href="{{ route('finish') }}" class="btn btn-warning ms-auto">Skip</a> --}}
                     </div>
-                </form>
-            </div>
+                </div>
+            </form>
+        </div>
         </div>
     </section>
-@endsection --}}
+@endsection
